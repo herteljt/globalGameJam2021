a36e6aaa25df73bf809459d02a5468d105e0f70e
Casey Brant <casey@caseybrant.com>
2021-01-30 15:07:52 -0600

    fake levels file
    NOTE: this isn't the REAL level format, just sketching to get some ideas down

---
+++ levels.lua
@@ -0,0 +1,17 @@
+levels = {}
+
+local o = 'empty space'
+local a = 'asteroid'
+local s = 'start'
+
+levels.first_level = {
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, a, o, o, o, o, o,
+  o, o, o, s, o, o, o, o, o, o, a, a, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+  o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o,
+}
