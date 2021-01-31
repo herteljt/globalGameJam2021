8649ced94b1298d5a06d76a9aa4e3a3f7c59f178
Casey Brant <casey@caseybrant.com>
2021-01-31 13:12:54 -0600

    intro text finished

--- dialogue.lua
+++ dialogue.lua
@@ -9,48 +9,56 @@ dialogue.introduction = {
   {
     name = "Z85000",
     avatar = "z85000",
-    text = "Running diagnostics."
-  },
-  {
-    name = "Z85000",
-    avatar = "z85000",
-    text = "Drone systems at 40%\n--> Operable"
-  },
-  {
-    name = "Z85000",
-    avatar = "z85000",
-    text = "Hyperwave signal strength at 70%\n--> Operable"
-  },
-  {
-    name = "Z85000",
-    avatar = "z85000",
-    text = "Communication screen\n--> Operable"
-  },
-  {
-    name = "Z85000",
-    avatar = "z85000",
-    text = "Internal mic\n--> Inoperable"
-  },
-  {
-    name = "Z85000",
-    avatar = "z85000",
-    text = "Initializing new user protocol."
+    text = "Running diagnostics.......\n.....OK."
   },
   {
     name = "Biff Starleap",
     avatar = "biff_enthusiastic",
-    text = "Hey there!"
+    text = "Thank you, budding Star Hero, for purchasing the brand new Z85000 rescue drone with hyperwave transistor control pod!"
+  },
+  {
+    name = "Biff Starleap",
+    avatar = "biff_enthusiastic",
+    text = "Your drone sits even now at the edge of known space, waiting for a distress call and your command to strike out for adventure!"
+  },
+  {
+    name = "Biff Starleap",
+    avatar = "biff_enthusiastic",
+    text = "Good luck to you! Perhaps I'll be seeing you at the next Space Rangers Meeting!"
   },
   {
     name = "Biff Starleap",
     avatar = "biff_tired",
-    text = "Ugh."
+    text = "Phew. Ok, ok. I've got three more today, let's get this over with. What's next?"
+  },
+  {
+    name = "Z85000",
+    avatar = "z85000",
+    text = "Loading mission briefing..."
   },
   {
     name = "Bored Teen",
     avatar = "bored_teenager",
-    text = "I guess I'm supposed to read you the rest of the mission briefing. Sigh."
+    text = "Uhh, Thank the moons of Flythyh you are here, oh most noble....?"
   },
+  {
+    name = "Bored Teen",
+    avatar = "bored_teenager",
+    text = "Oh, this one's microphone isn't working. OK, uh, most noble Default_Name. A ship is stranded in this sector and needs your help."
+  },
+  {
+    name = "Bored Teen",
+    avatar = "bored_teenager",
+    text = "Uhh, Navigate your drone through the asteroid field until close enough to receive their transmission, then rescue them."
+  },
+  {
+    name = "Bored Teen",
+    avatar = "bored_teenager",
+    text = "You are their, uh, only hope."
+  },
+}
+
+dialogue.asdfasdf = {
   {
     name = "Stranded Alien",
     avatar = "alien_excited",
